a2ae552ecba026bb823b330903fb75cfa6b2214d
Gus <gus@mythril.co>
2015-08-26 16:53:38 -0700

    Add fullscreen view in reader

--- app/js/display.js
+++ app/js/display.js
@@ -428,6 +428,9 @@ var Display = function(data) {
             }
         });
 
+        $('.main-view > img').on('click', function () {
+            toggleFullScreen($('.main-view')[0]);
+        });
     };
 
     this.startLoading = function (caller, process) {
@@ -473,4 +476,29 @@ var Display = function(data) {
         }
 
     };
+};
+
+var toggleFullScreen = function (elem) {
+    // ## The below if statement seems to work better ## if ((document.fullScreenElement && document.fullScreenElement !== null) || (document.msfullscreenElement && document.msfullscreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if ((document.fullScreenElement !== undefined && document.fullScreenElement === null) || (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) || (document.mozFullScreen !== undefined && !document.mozFullScreen) || (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)) {
+        if (elem.requestFullScreen) {
+            elem.requestFullScreen();
+        } else if (elem.mozRequestFullScreen) {
+            elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullScreen) {
+            elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+        } else if (elem.msRequestFullscreen) {
+            elem.msRequestFullscreen();
+        }
+    } else {
+        if (document.cancelFullScreen) {
+            document.cancelFullScreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
 };
