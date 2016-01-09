d1065bccdb669bd49b406ebeec7d911690a55619
Stuart Runyan <shrunyan@gmail.com>
2016-01-09 13:27:53 -0800

    Organize server into directory under src. Update paths.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,16 +5,15 @@ var searchHandler = require('../search');
 
 module.exports = exports = function(app) {
 
-
     app.use('/search/*', searchHandler.update);
-    
+
     app.post('/search/alias/:term?/', searchHandler.alias);
     app.get('/search/alias/:term?/', searchHandler.alias);
-    
+
 
     app.post('/search/genres/', searchHandler.genres);
     app.get('/search/genres/', searchHandler.genres);
-    
+
     app.post('/search/sorts/', searchHandler.sorts);
     app.get('/search/sorts/', searchHandler.sorts);
 };
